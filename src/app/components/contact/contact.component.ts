013942168c100a55f3cfb28f1836623a6f4a594c
DanRivSa <danielrivero2000@gmail.com>
2020-07-01 01:51:16 -0400

    cosmetic update on all components

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {fade} from '../../animations/MainAnimations';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
+  animations:[fade]
 })
 export class ContactComponent implements OnInit {
 
