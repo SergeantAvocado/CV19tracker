999f69bafa6e32a700b59137e31540bc9745c192
DanRivSa <danielrivero2000@gmail.com>
2020-06-30 22:22:04 -0400

    added contact component and month feature

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
 import {SummaryComponent} from "./components/summary/summary.component";
+import {ContactComponent} from './components/contact/contact.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
-  {path:'cases',component:SummaryComponent}
+  {path:'cases',component:SummaryComponent},
+  {path:'DeveloperContact',component:ContactComponent},
 ];
 
 @NgModule({
